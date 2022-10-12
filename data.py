d3652474cf0207d91e74df4486237dc87fd0c5e3
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-10-12 22:04:29 +0700

    Draft

---
+++ data.py
@@ -0,0 +1,12 @@
+foods = {
+    'name': {
+        'price': 100
+    }
+}
+
+people = {
+    'name' : {
+        'foods': ['NN'],
+        'price': [100]
+    }
+}
--- index.py
+++ index.py
@@ -1,16 +1,40 @@
 from flask import Flask,render_template,request,session,redirect
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 
+people = list()
+foods = dict()
+session['people'] = False
+
 @app.route('/')
 def index():
+    
     return render_template('index.html')
 
 @app.route('/addpeople',methods=['GET', 'POST'])
 def addpeople():
+    if request.method == 'POST':
+        name = request.form['name']
+        people.append(name)
 
-    render_template('index.html')
+        print(people)
+        session['people'] = people
+        
+    return render_template('index.html',name=name)
+
+@app.route('/addfood',methods=['GET', 'POST'])
+def addfood():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        foods.update({name:{"price":price}})
+
+    print(foods)
+        
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
