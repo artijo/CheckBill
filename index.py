4cbce6861010ba4996bba1d0c63c96bf718ca7b1
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-10-12 14:37:41 +0700

    Started Project

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 from flask import Flask,render_template,request,session,redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/addpeople',methods=['GET', 'POST'])
+def addpeople():
+
+    render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
