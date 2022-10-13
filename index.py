023de8d112b0ffb09fee5929ac4ffc8eca56d132
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-10-13 17:48:25 +0700

    Update index.py

--- index.py
+++ index.py
@@ -4,68 +4,66 @@ from flask import Flask,render_template,request,session,redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 
-people = list() #เก็บข้อมูลชื่อผู้ใช้
-menu = list() #เก็บข้อมูลชื่ออาหาร
-foods = dict() #เก็บข้อมูลชื่ออาหาร,ชื่อผู้ใช้ที่เลือกอาหารนี้และราคาของอาหาร
-result = dict() #เก็บข้อมูลชื่อผู้ใช้ และราคาที่ต้องจ่าย
-
 @app.route('/')
 def index():
-    session['people'] = False
+
+    session['forlist'] = False
+    session['people'] = list()
+    session['menu'] = list()
+    session['foods'] = dict()
+    session['result'] = dict()
     return render_template('index.html')
 
 @app.route('/addpeople',methods=['GET', 'POST'])
 def addpeople():
+    session['forlist'] = True
     if request.method == 'POST':
         name = request.form['name']
-        if name not in people:
-            people.append(name)
-
-        # print(people)
-        session['people'] = people
+        if name not in session['people']:
+            session['people'].append(name)
         
     return render_template('index.html',name=name)
 
 @app.route('/addfood',methods=['GET', 'POST'])
 def addfood():
+    session['forlist'] = True
     if request.method == 'POST':
         name = request.form['name']
         price = request.form['price']
         peoples = request.form.getlist('people')
-        menu.append(name)
-        foods.update({name:{"people":peoples,"price":price}})
+        session['menu'].append(name)
+        session['foods'].update({name:{"people":peoples,"price":price}})
 
     # session['menu'] = menu
-    for i in foods :
-        for x in menu :
+    for i in session['foods'] :
+        for x in session['menu'] :
             if i == x :
-                countitem = len(foods[i]['people'])  #จำนวนคนในการเลือกอาหารนั้น
-                hhh = int(foods[x]['price']) / countitem  # ราคาหารจำนวนอาหาร
+                countitem = len(session['foods'][i]['people'])  #จำนวนคนในการเลือกอาหารนั้น
+                hhh = int(session['foods'][x]['price']) / countitem  # ราคาหารจำนวนอาหาร
                 hhh = round(hhh,2)
-    for y in people:
-        if y in foods[i]['people']:
+    for y in session['people']:
+        if y in session['foods'][i]['people']:
             try:
-                dt = result[y]
+                dt = session['result'][y]
                 print('...',dt)
                 dt = dt+hhh
-                result.update({y:dt})
+                session['result'].update({y:dt})
             except KeyError:
-                result.update({y:hhh})
+                session['result'].update({y:hhh})
 
 
     # print(menu)
     # print(countitem)
     # print(hhh)
 
-    # for i in result:
-    #     print(i,'\t\t\t',result[i])
+    for i in session['result']:
+        print(i,'\t\t\t',session['result'][i])
     # print(people)
-    session['result'] = result
     return render_template('index.html')
 
 @app.route('/clare')
 def clare():
-    session['people'] = session['result'] = result = foods = menu = people = None
+    session['people'] = session['result'] = session['foods'] = session['menu'] = None
     return redirect('/')
 
 
