12f38ccc6ba43dfd31a2c001b5e7d4c386088310
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-10-13 17:16:02 +0700

    V0.5 Beta

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ from flask import Flask,render_template,request,session,redirect
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 
-people = list()
-menu = list()
-foods = dict()
-result = dict()
+people = list() #เก็บข้อมูลชื่อผู้ใช้
+menu = list() #เก็บข้อมูลชื่ออาหาร
+foods = dict() #เก็บข้อมูลชื่ออาหาร,ชื่อผู้ใช้ที่เลือกอาหารนี้และราคาของอาหาร
+result = dict() #เก็บข้อมูลชื่อผู้ใช้ และราคาที่ต้องจ่าย
 
 @app.route('/')
 def index():
@@ -18,7 +18,8 @@ def index():
 def addpeople():
     if request.method == 'POST':
         name = request.form['name']
-        people.append(name)
+        if name not in people:
+            people.append(name)
 
         # print(people)
         session['people'] = people
@@ -30,16 +31,17 @@ def addfood():
     if request.method == 'POST':
         name = request.form['name']
         price = request.form['price']
-        people = request.form.getlist('people')
+        peoples = request.form.getlist('people')
         menu.append(name)
-        foods.update({name:{"people":people,"price":price}})
+        foods.update({name:{"people":peoples,"price":price}})
 
     # session['menu'] = menu
     for i in foods :
         for x in menu :
             if i == x :
-                countitem = len(foods[i]['people']) #จำนวนคนในการเลือกอาหารนั้น
-                hhh = int(foods[x]['price']) / countitem # ราคาหารจำนวนอาหาร
+                countitem = len(foods[i]['people'])  #จำนวนคนในการเลือกอาหารนั้น
+                hhh = int(foods[x]['price']) / countitem  # ราคาหารจำนวนอาหาร
+                hhh = round(hhh,2)
     for y in people:
         if y in foods[i]['people']:
             try:
@@ -53,13 +55,18 @@ def addfood():
 
     # print(menu)
     # print(countitem)
-    print(hhh)
+    # print(hhh)
 
-    for i in result:
-        print(i,'\t\t\t',result[i])
+    # for i in result:
+    #     print(i,'\t\t\t',result[i])
     # print(people)
-        
+    session['result'] = result
     return render_template('index.html')
+
+@app.route('/clare')
+def clare():
+    session['people'] = session['result'] = result = foods = menu = people = None
+    return redirect('/')
 
 
 if __name__ == "__main__":
